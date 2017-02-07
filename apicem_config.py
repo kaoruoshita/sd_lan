28bba6eb7568e3d78917014d88f1ba9eedd3aaac
Kaoru Oshita <kaoru.oshita@Kaorus-MacBook-Pro.local>
2017-02-07 13:01:30 +0900

    initial commit sd_lan

---
+++ apicem_config.py
@@ -0,0 +1,12 @@
+# Step 1
+# Change apic-em IP to the one you are using
+ip =  "sandboxapic.cisco.com"
+# ip = "devnetapi.cisco.com/sandbox/apic_em"
+
+
+# Step 2
+# Eneter user name and password to get a service ticket
+# If you assign username, password and version here you don't need to pass parameter when calling
+username = "devnetuser"
+password = "Cisco123!"
+version = "v1"
---
+++ main.py
@@ -0,0 +1,59 @@
+from apicem import *
+from datetime import datetime
+import json
+
+def get_resources():
+#def get_resources(event, context):
+    response = get(api="network-device")
+
+    response_json = response.json()
+    devices = response_json["response"]
+    print list(format_resources(devices))
+    return list(format_resources(devices))
+def format_resources(resources):
+     for resource in resources:
+         try:
+             yield format_resource(resource)
+         except KeyError:
+             continue
+
+def format_resource(resource):
+    
+    location_id = resource["location"]
+    
+    location_details = get_location_details(location_id)
+    return {
+        'base': {
+            'name': resource["hostname"],
+            'provider_created_at': datetime.utcnow().isoformat() + "Z"
+        },
+        'id': resource.get("managementIpAddress"),
+        'type': 'appliance',
+        'details': {
+            'appliance': {
+                "type_id": '1',
+                "family" : resource["family"],
+                "location": location_details
+            }
+        }
+    }
+
+def get_location_details(location_id):
+    try:
+        print location_id
+        response = get(api="location/"+ location_id)
+        response_json = response.json()
+        data = response_json["response"]
+        latitude, longitude =data["geographicalAddress"].split("/")
+    
+        return {
+                "city": {
+                  "latitude": latitude,
+                  "longitude": longitude,
+                  "name": data["locationName"]
+                }
+        }
+    except:
+        pass
+
+get_resources()
